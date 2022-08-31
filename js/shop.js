fc9a13199cff5e28e3fe3522a824604df6ad7060
begopla <begonapr@gmail.com>
2022-08-31 13:34:37 +0400

    Nivell 2 completed

--- js/shop.js
+++ js/shop.js
@@ -6,72 +6,78 @@ var cartList = [];
 var cart = [];
 
 var total = 0;
+let showTotal =0;
+let itemsInCart = document.getElementById('count_product');
+let cartItems = document.getElementById('cart_list')
+let totalCost = document.getElementById('total_price')
+
 
 // Exercise 1
-function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
-products.forEach((product)=>{
-    if(product.id== id){
-        cartList.push(product)
-    }
-})
-calculateTotal(id)
+// function buy(id) {
+//     // 1. Loop for to the array products to get the item to add to cart
+//     // 2. Add found product to the cartList array
+// products.forEach((product)=>{
+//     if(product.id== id){
+//         cartList.push(product)
+//     }
+// })
+// calculateTotal(id)
 
-//Display number of item in shooping cart
+// //Display number of item in shooping cart
 
-let itemsInCart = document.getElementById('count_product');
-itemsInCart.innerHTML = cartList.length;
-}
+// let itemsInCart = document.getElementById('count_product');
+// itemsInCart.innerHTML = cartList.length;
+// }
 
 // Exercise 2
 function cleanCart() {
     cartList.length=0;
+    cart=[]
     total=0;
 
     //update innerHTML
-    let cartItems = document.getElementById('cart_list')
-    let totalCost = document.getElementById('total_price')
+    
     cartItems.innerHTML = ''
     totalCost.innerHTML=0;
+    itemsInCart.innerHTML = cartList.length;
 }
 
 // Exercise 3
-function calculateTotal(id) {
-    // Calculate total price of the cart using the "cartList" array
-    cartList.forEach((product)=>{
-        if(product.id== id){
-        total += product.price;
-        }
-    })
-}
+// function calculateTotal(id) {
+//     // Calculate total price of the cart using the "cartList" array
+//     cartList.forEach((product)=>{
+//         if(product.id== id){
+//         total += product.price;
+//         }
+//     })
+// }
 
 // Exercise 4
-function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-    cart=[];
+// function generateCart() {
+//     // Using the "cartlist" array that contains all the items in the shopping cart, 
+//     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+//     cart=[];
     
-    cartList.forEach(product=>{
-    //if cart is empty push the first item 
-        if(cart.length>0){
-            //I need to check if there is any reapeted item inside cartList
-            let repeated = cart.includes(product)
-            if(repeated == true){
-                product.quantity ++;
-            }else{
-                product.quantity = 1;
-                cart.push(product)
-            }
-        }
-        else{
-            product.quantity=1;
-            cart.push(product);
-        }
-            });
-    applyPromotionsCart()
+//     cartList.forEach(product=>{
+//     //if cart is empty push the first item 
+//         if(cart.length>0){
+//             //I need to check if there is any reapeted item inside cartList
+//             let repeated = cart.includes(product)
+//             if(repeated == true){
+//                 product.quantity ++;
+//             }else{
+//                 product.quantity = 1;
+//                 cart.push(product)
+//             }
+//         }
+//         else{
+//             product.quantity=1;
+//             cart.push(product);
+//         }
+//             });
+//     applyPromotionsCart()
 
-}
+// }
 
 // Exercise 5
 function applyPromotionsCart() {
@@ -93,13 +99,11 @@ function applyPromotionsCart() {
 
 // Exercise 6
 function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
-    let cartItems = document.getElementById('cart_list')
-    let totalCost = document.getElementById('total_price')
-    let showTotal =0;
+    // Fill the shopping cart modal manipulating the shopping cart dom   
+    showTotal =0;
     cartItems.innerHTML = ''
     
-    generateCart()
+    applyPromotionsCart()
     cart.forEach((item)=>{
         cartItems.innerHTML += `
         <tr>
@@ -110,11 +114,10 @@ function printCart() {
 		</tr>
         `;
         showTotal+= item.subTotal;
+        console.log(item)
     })
     totalCost.innerHTML=showTotal;
-
 }
-
 
 // ** Nivell II **
 
@@ -123,9 +126,9 @@ function addToCart(id) {
     // Refactor previous code in order to simplify it 
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
-
     products.forEach((product)=>{
         if(product.id == id){
+            cartList.push(product)
             if(cart.length>0){
                 //I need to check if there is any reapeted item inside cart
                 let repeated = cart.includes(product)
@@ -141,20 +144,36 @@ function addToCart(id) {
                 cart.push(product);
             }   
         }
-    })
+    });
     
-
-    //Display number of item in shooping cart
-
-    let itemsInCart = document.getElementById('count_product');
-        itemsInCart.innerHTML = cart.length;
+    //Display number of item in shooping cart  
+        itemsInCart.innerHTML = cartList.length;
     
 }
 
 // Exercise 8
 function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+    // 1. Loop for to the array products to get the item to remove from cart
+    // 2. Remove found product from cartList/cart array
+
+    let selectedProduct = cart.find(item => item.id == id)
+
+    let productIndex = cart.findIndex(item => item.id == id)
+    let cartListIndex = cartList.findIndex(item =>item.id==id)
+    console.log(selectedProduct, productIndex)
+
+    //Remove item from cart
+    if(productIndex>=0 && selectedProduct.quantity>1){
+        cart[productIndex].quantity --;
+        console.log('new cart:',cart)
+    }else if (productIndex >=0 && selectedProduct.quantity ==1){
+        cart.splice(productIndex,1)
+        console.log('new cart:', cart)
+    }
+
+    //Remove item from cartList to update number of items in cart
+    cartList.splice(cartListIndex,1);
+    itemsInCart.innerHTML = cartList.length;
 }
 
 function open_modal(){
